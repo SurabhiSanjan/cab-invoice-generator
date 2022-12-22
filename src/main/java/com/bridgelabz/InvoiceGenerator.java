50bcd35241ccf5716d8a6f71d4eb6266c0ab208d
surabhisanjan <surabhisanjan@gmail.com>
2022-12-22 18:51:09 +0530

    Initializing Cab Invoice Generator problem day -30

---
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+
+public class InvoiceGenerator {
+    static final double COST_PER_KM = 10;
+    static final int COST_PER_MINUTE = 1;
+    static final double MINIMUM_FARE = 5;
+
+    public static void main(String[] args )
+    {
+        System.out.println( "Welcome to Cab Invoice Generator" );
+    }
+    public static double totalFare(double distance, int time) {
+        double totalCostForDist = distance * COST_PER_KM;
+        int totalCostForTime = time * COST_PER_MINUTE;
+        double totalFare = totalCostForDist + totalCostForTime;
+
+        if (totalFare < MINIMUM_FARE)
+            return MINIMUM_FARE;
+        else return totalFare;
+    }
+}
